1bb7083477b47e9173cf2f493d3a812637096d46
Nassar, Tamer <tamer.nassar@intel.com>
2022-01-25 01:30:13 +0200

    clean pychache

--- examples/DFSEmulator.py
+++ examples/DFSEmulator.py
@@ -2,5 +2,5 @@ from Emulator.SearchEmulator import SearchEmulator
 from SearchAlgorithms.SearchAlgorithms import DFS
 
 
-SearchEmulator(gif_path='../gifs/maze.gif').emulate('../mazes/maze.png', DFS())
+SearchEmulator(gif_path='../gifs/maze.gif').emulate(image='../mazes/maze.png', algorithm=DFS())
 
